283262dc6d078b80a21e736e4d2ad0ecfbae42bd
Hawi Ketema <85627898+lattekiki@users.noreply.github.com>
2026-02-04 21:32:28 +0300

    Update App.vue

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -3,8 +3,8 @@ import {ref} from 'vue'
 const name = ref('')
 </script>
 
-
 <template>
   <input v-model="name" placeholder="Type your name...">
-  <p>Hello {{ name }}</p>
+  <h1 v-if="name.length > 0"> Hi Everyone 👋, This is {{ name }}</h1>
+  <p v-else> Please enter your name to see the greeting message.</p>
 </template>
