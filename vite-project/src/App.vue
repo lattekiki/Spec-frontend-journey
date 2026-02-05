fa5576276a4d92ebeba37a505d7fdf48c176ad4f
Hawi Ketema <85627898+lattekiki@users.noreply.github.com>
2026-02-05 18:08:35 +0300

    Update App.vue

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,10 +1,44 @@
 <script setup>
-import {ref} from 'vue'
-const name = ref('')
+import UserCard from './components/UserCard.vue'
 </script>
 
 <template>
-  <input v-model="name" placeholder="Type your name...">
-  <h1 v-if="name.length > 0"> Hi Everyone 👋, This is {{ name }}</h1>
-  <p v-else> Please enter your name to see the greeting message.</p>
+  <div class = "container">
+  <h1>Team Members</h1>
+    <div class="member_list">
+      <UserCard class = "member-card" name = "👧Melody" role = "Developer"/>
+      <UserCard class = "member-card" name = "👦John" role = "Tester"/>
+      <UserCard class = "member-card" name = "👧Alice" role = "Product Manager"/>
+    </div>
+</div>
+>>>>>>> a1b8aa9 (Day 3 component task)
 </template>
+<style>
+h1{
+    margin-bottom: 40px;
+    text-align: center;
+    color: rgb(224, 237, 237);
+    font-size: 40px;
+}
+.container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    justify-content: center;
+    min-height: 100vh;
+}
+
+.member_list{
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  justify-content: center; 
+}
+
+.member-card{
+  font-size: 20px;
+}
+
+</style>
+
