0c1a046ae5d85cfc520ca31b12ad0ec3c157b9fb
Hawi Ketema <85627898+lattekiki@users.noreply.github.com>
2026-02-05 21:12:59 +0300

    Update UserCard.vue

--- vite-project/src/components/UserCard.vue
+++ vite-project/src/components/UserCard.vue
@@ -1,15 +1,26 @@
 
 <script setup>
  const props = defineProps([
-    'name','role'
+    'name','role','status'
  ])
+ const emits = defineEmits(['view-profile'])
+ const handleClick = () => {
+    emits('view-profile', props.name)
+ }
 </script>
 
  <template>
-    <div class = "user-card">
+    <div :class="['user-card', status]">
         <h3>{{ name }}</h3>
         <p>{{ role }}</p>
+        <p v-if="status === 'Online'"> 🟢
+
+        </p>
+        <p v-else> 🔴</p>
+        <button class = "btn"@click="handleClick">View Profile</button>
     </div>
+
+    
  </template>
 
 
@@ -19,8 +30,27 @@
     border: 2px solid #6b1250;
     border-radius: 20px;
     padding: 20px;
-    width: 330px; 
-    margin-left: 35px;
+    width: 300px; 
+    margin-left: 50px;
+    text-align: center;
+    color: white;
+    box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+}
+
+.user-card.Online{
+    border-color: #4CAF50;
+}
+.user-card.Offline{
+    border-color: #6b1250;
+}
+.btn{
+     background-color: rgb(24, 3, 50);
+     justify-content: center;
+    border: 2px solid #6b1250;
+    border-radius: 20px;
+    padding: 20px;
+    width: 150px; 
+    font-size: 20px;
     text-align: center;
     color: white;
     box-shadow: 0 4px 8px rgba(0,0,0,0.2);
